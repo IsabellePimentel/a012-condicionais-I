a3fd318bd87db094226679b59d2bf7bff3d9aac4
isabellepimentel <isabellechavespimentel@gmail.com>
2022-09-06 15:51:37 -0300

    Exercicio Condicionais

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+const nacionalidade = prompt("Escreva aqui sua nacionalidade");
+
+if(nacionalidade === "brasileira"){
+    console.log("Nacionalidade Brasileira")
+}else if(nacionalidade === "argentina"){
+    console.log("Nacionalidade Argentina")
+}else if(nacionalidade === "uruguaia"){
+    console.log("Nacionalidade Uruguaia")
+}else if(nacionalidade === "chilena"){
+    console.log("Nacionalidade chilena")
+}else if(nacionalidade === "colombiana"){
+    console.log("Nacionalidade Colombiana")
+}else{
+    console.log("Nacionalidade não encontrada")
+}
